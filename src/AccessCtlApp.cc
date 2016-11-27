016041d750e50e24baf48242321837fa64638f0a
xanter <romanov6416@yandex.ru>
2016-11-27 17:59:21 +0300

    todo: test h1 ping h2

--- src/AccessCtlApp.cc
+++ src/AccessCtlApp.cc
@@ -5,21 +5,85 @@
 #include "AccessCtlApp.hh"
 
 #include <glog/logging.h>
-//#include <Controller.hh>
-#include "api/PacketMissHandler.hh"
+
+#include <sstream>
+#include <boost/lexical_cast.hpp>
+
 #include "api/Packet.hh"
-#include "types/ethaddr.hh"
-#include "api/TraceablePacket.hh"
+#include "api/PacketMissHandler.hh"
 #include "oxm/openflow_basic.hh"
+#include "types/ethaddr.hh"
+
+#include "Controller.hh"
+#include "SwitchConnection.hh"
+#include "Flow.hh"
+#include "Common.hh"
+
 
 REGISTER_APPLICATION(AccessCtlApp, {"controller", ""})
 
-void AccessCtlApp::init(Loader *loader, const Config& config)
+
+
+void AccessCtlApp::init(Loader *l, const Config& config)
 {
+	loader = l;
+	parseConfig(config);
 	Controller* ctrl = Controller::get(loader);
-//	QObject::connect(ctrl, &Controller::switchUp, this, &AccessCtlApp::onSwitchUp);
-	QObject::connect(ctrl, &Controller::switchDown, this, &AccessCtlApp::onSwitchDown);
-	LOG(INFO) << "Hello, world!";
+
+//	LOG(INFO) << "Homework app init";
+
+	uint8_t table_no = ctrl->reserveTable(); // Your must push flowmod in this table
+	table_no = table_no;
+	ctrl->registerHandler(
+		"access-control",
+		[=](SwitchConnectionPtr connection) {
+
+			return [=](Packet& pkt, FlowPtr flw, Decision decision) {
+
+				// Write your code here
+				//Below example code
+				Session s(pkt);
+				LOG(INFO) << s.srcEth << " " << s.dstEth;
+
+				if (this->hasSymmetricSession(s)) {
+					LOG(INFO) << "has access (symmetric)";
+					return decision;
+				}
+
+				if (this->hasAccess(s, flw)) {
+					LOG(INFO) << "has access (new)";
+					this->addSession(s, flw);
+					return decision;
+				}
+
+				// TODO: send ICMP error
+				LOG(INFO) << "has not access";
+				return decision.idle_timeout(std::chrono::seconds::zero()).drop().return_();
+//				LOG(INFO) << "PacketIn captured";
+//
+//				ethaddr eth_dst = pkt.load(oxm::eth_dst());
+//				if (eth_dst.to_number() % 2 == 0){
+//					// I want to drop all packet that ended with zero bits
+//					of13::FlowMod fm;
+//					fm.command(of13::OFPFC_ADD);
+//					fm.table_id(table_no); //  Push your flow in this table
+//
+//					fm.priority(1); // Priority must be higher than 0
+//
+//					std::stringstream ss;
+//					ss << eth_dst;
+//
+//					of13::EthDst* ethDstToFlowMod = new of13::EthDst(
+//						EthAddress( ss.str() )
+//					); // Sorry for this
+//
+//					fm.add_oxm_field(ethDstToFlowMod);
+//					connection->send(fm);
+//				}
+//
+//				return decision;
+			};
+		});
 }
 
 void AccessCtlApp::onSwitchUp(SwitchConnectionPtr ofconn, of13::FeaturesReply fr)
@@ -31,3 +95,100 @@ void AccessCtlApp::onSwitchDown(SwitchConnectionPtr ofconn)
 {
 	LOG(INFO) << "Look! This is no switch";
 }
+
+void AccessCtlApp::parseConfig(const Config &config) {
+	auto appCfg = config_cd(config, "access-control");
+	for (auto userAccess : appCfg["users-permissions"].object_items()) {
+		auto &userMAC = userAccess.first;
+		auto hostCfg = userAccess.second.object_items();
+		parseUserPermission(hostCfg, permUsers[userMAC]);
+	}
+//	auto hostCfg = appCfg["default-permissions"].object_items();
+	parseUserPermission(appCfg["default-permissions"], defaultPermission);
+}
+
+
+void AccessCtlApp::parseUserPermission(const json11::Json & cfg, UserPermission &up) {
+	for (auto hostAccessInfo : cfg.object_items()) {
+		auto dstMAC = hostAccessInfo.first;
+		AccessInfo & access = up[dstMAC];
+		for (auto tcpPort : hostAccessInfo.second["tcp-ports"].array_items())
+			access.tcpPorts.insert(tcpPort.int_value());
+		for (auto udpPort : hostAccessInfo.second["udp-ports"].array_items())
+			access.udpPorts.insert(udpPort.int_value());
+		for (auto proto : hostAccessInfo.second["protocols"].array_items())
+			access.protocols.insert(proto.string_value());
+//			LOG(INFO) << *(permUsers[userMAC][dstMAC].tcpPorts.begin());
+//			LOG(INFO) << *(permUsers[userMAC][dstMAC].udpPorts.begin());
+	}
+}
+
+
+
+bool AccessCtlApp::hasAccess(const Session &s, FlowPtr flw) {
+	auto userEthIt = permUsers.find(s.srcEth);
+	if (userEthIt == permUsers.cend())
+		return false;
+
+	if (hasPermission(s, userEthIt->second))
+		return true;
+
+	if (hasPermission(s, defaultPermission))
+		return true;
+
+	return false;
+}
+
+
+
+bool AccessCtlApp::hasPermission(const Session &s, UserPermission &up) {
+
+	auto dstEthIt = up.find((s.dstEth));
+	if (dstEthIt == up.cend())
+		return false;
+
+	AccessInfo & accessInfo = dstEthIt->second;
+
+	LOG(INFO) << *(defaultPermission["ff:ff:ff:ff:ff:ff"].protocols.cbegin());
+	LOG(INFO) << s.ipProto;
+
+	if (s.ethType == ethtypes::ARP) {
+		return accessInfo.protocols.find("ARP") != accessInfo.protocols.cend();
+	} else if (s.ethType == ethtypes::IPv4 || s.ethType == ethtypes::IPv6) {
+		if (s.ipProto == ipprotos::ICMP) {
+			return accessInfo.protocols.find("ICMP") != accessInfo.protocols.cend();
+		} else if (s.ipProto == ipprotos::TCP) {
+			return accessInfo.tcpPorts.find(s.dstAppPort) == accessInfo.tcpPorts.cend();
+		} else if (s.ipProto == ipprotos::UDP) {
+			return accessInfo.udpPorts.find(s.dstAppPort) == accessInfo.udpPorts.cend();
+		} else
+			LOG(WARNING) << "unknown ip protocol " << s.ethType;
+	} else
+		LOG(WARNING) << "unknown eth type " << s.ethType;
+	return false;
+}
+
+
+bool AccessCtlApp::hasSymmetricSession(const Session & incomingSession) {
+	for (auto sInfo : curSessions)
+		if (incomingSession.isSymmetric(sInfo.second))
+			return true;
+	return false;
+}
+
+
+void AccessCtlApp::addSession(const Session & s, FlowPtr flw) {
+//	curSessions[flw->cookie()] = s;
+	curSessions.insert(std::make_pair(flw->cookie(), s));
+}
+
+
+void AccessCtlApp::delSession(FlowPtr flw) {
+	curSessions.erase(flw->cookie());
+}
+
+
+
+
+
+
--- src/AccessCtlApp.hh
+++ src/AccessCtlApp.hh
@@ -3,17 +3,109 @@
 //
 
 #pragma once
+
+#include <boost/lexical_cast.hpp>
+#include <types/ethaddr.hh>
+#include "Flow.hh"
+#include <oxm/openflow_basic.hh>
+#include "PacketParser.hh"
 #include "Application.hh"
 #include "Controller.hh"
 #include "Loader.hh"
+#include <unordered_map>
+#include <unordered_set>
 //#include "Switch.hh"
-//#include "fluid/of13"
+//#include "fluid/of13
+
+
+namespace ethtypes{
+	constexpr unsigned int IPv4 = 0x0800;
+	constexpr unsigned int IPv6 = 0x86dd;
+	constexpr unsigned int ARP = 0x0806;
+};
+
+
+namespace ipprotos {
+	constexpr unsigned int TCP = 0x06;
+	constexpr unsigned int UDP = 0x11;
+	constexpr unsigned int ICMP = 0x01;
+};
+
+
+std::string ethAddrToString(const ethaddr & ethAddr) {
+	return std::string(boost::lexical_cast<std::string>(ethAddr));
+}
+
+
+struct Session {
+	std::string srcEth;
+	std::string dstEth;
+	int ethType;
+	int ipProto;
+	int srcAppPort;
+	int dstAppPort;
+
+	Session(Packet & pkt) {
+		srcEth = ethAddrToString(pkt.load(oxm::eth_src()));
+		dstEth = ethAddrToString(pkt.load(oxm::eth_dst()));
+		ethType = int(pkt.load(oxm::eth_type()));
+
+		LOG(INFO) << "ethtype " << ethType;
+
+		if (ethType == ethtypes::IPv4 || ethType == ethtypes::IPv6) {
+			ipProto = pkt.load(oxm::ip_proto());
+			if (ipProto == ipprotos::TCP) {
+				srcAppPort = pkt.load(oxm::tcp_src());
+				dstAppPort = pkt.load(oxm::tcp_dst());
+			} else if (ipProto == ipprotos::UDP) {
+				srcAppPort = pkt.load(oxm::udp_src());
+				dstAppPort = pkt.load(oxm::udp_dst());
+			} else {
+				srcAppPort = dstAppPort = -1;
+			}
+		} else {
+			ipProto = srcAppPort = dstAppPort = -1;
+		}
+	}
+
+	bool isSymmetric(const Session & s) const {
+		return srcEth == s.dstEth && dstEth == s.srcEth &&
+		       ethType == s.ethType && ipProto == s.ipProto &&
+		       srcAppPort == s.dstAppPort && dstAppPort == s.srcAppPort;
+	}
+};
+
+
+
+struct AccessInfo {
+	std::unordered_set<int> tcpPorts;
+	std::unordered_set<int> udpPorts;
+	std::unordered_set<std::string> protocols;
+};
+
+typedef std::unordered_map<std::string, AccessInfo> UserPermission;
+//typedef std::unordered_map<std::string, UserPermissions> Perm;
+
 
 class AccessCtlApp : public Application {
 	SIMPLE_APPLICATION(AccessCtlApp, "access-control")
+
+	Loader* loader;
+	std::unordered_map<std::string, UserPermission> permUsers;
+	UserPermission defaultPermission;
+	std::unordered_map<uint64_t, Session> curSessions;
+
+	void parseConfig(const Config & config);
+	void parseUserPermission(const json11::Json & cfg, UserPermission & up);
+	bool hasSymmetricSession(const Session & s);
+	bool hasAccess(const Session &s, FlowPtr flw);
+	bool hasPermission(const Session & s, UserPermission & up);
+	void addSession(const Session & s, FlowPtr flw);
+	void delSession(FlowPtr flw);
 public:
 	void init(Loader* loader, const Config& config) override;
 public slots:
 	void onSwitchUp(SwitchConnectionPtr ofconn, of13::FeaturesReply fr);
 	void onSwitchDown(SwitchConnectionPtr ofconn);
+
 };
